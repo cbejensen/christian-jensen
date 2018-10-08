f45cdf0d0e6353af95e6dd36673f5e0f84924856
Christian Jensen <cbejensen@gmail.com>
2018-10-08 15:24:51 -0600

    refactoring to prevent styled-components generating so many classes

--- src/components/TriangleCurve.js
+++ src/components/TriangleCurve.js
@@ -6,6 +6,12 @@ import styled from 'styled-components'
 export default class TriangleCurve extends React.Component {
   render() {
     const triangleSize = this.props.triangleSize
+    let xReference = 'left',
+      yReference = 'top'
+    if (this.props.corner) {
+      xReference = this.props.corner.includes('right') ? 'right' : 'left'
+      yReference = this.props.corner.includes('bottom') ? 'bottom' : 'top'
+    }
     return (
       <CornerCurveScatter
         width={this.props.width}
@@ -15,15 +21,16 @@ export default class TriangleCurve extends React.Component {
         corner={this.props.corner}
       >
         {items => {
-          // console.log(items)
           return (
             <RandomRotations itemsToRotate={items} rotationInterval={200}>
               {rotations => (
                 <React.Fragment>
                   {items.map((item, i) => (
                     <StyledSVG
-                      corner={this.props.corner}
-                      item={item}
+                      xReference={xReference}
+                      yReference={yReference}
+                      x={item.x}
+                      y={item.y}
                       size={triangleSize}
                       key={i}
                     >
@@ -44,30 +51,17 @@ export default class TriangleCurve extends React.Component {
 }
 
 const StyledSVG = styled.svg.attrs({
-  viewBox: '0 0 100 100'
+  viewBox: '0 0 100 100',
+  style: props => ({
+    [props.xReference]: `calc(${props.x}% - ${props.size}px)`,
+    [props.yReference]: `calc(${props.y}% - ${props.size}px)`,
+    width: `${props.size}px`,
+    height: `${props.size}px`
+  })
 })`
   position: absolute;
   overflow: visible;
-  ${({ corner, item, size }) => getCoords(corner, item.x, item.y, size)};
 `
-
-function getCoords(corner, itemX, itemY, size) {
-  let topOrBottom, rightOrLeft
-  if (!corner) {
-    // no corner set? use top left as default
-    topOrBottom = 'top'
-    rightOrLeft = 'left'
-  } else {
-    topOrBottom = corner.includes('bottom') ? 'bottom' : 'top'
-    rightOrLeft = corner.includes('right') ? 'right' : 'left'
-  }
-  return `
-    ${topOrBottom}: calc(${itemY}% - ${size}px);
-    ${rightOrLeft}: calc(${itemX}% - ${size}px);
-    width: ${size}px;
-    height: ${size}px;
-  `
-}
 
 const Triangle = styled.polygon.attrs({
   points: '50 0 100 100 0 100',

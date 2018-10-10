fb09671754d27a58adcd4ac3db591ae2bf9e2946
Christian Jensen <cbejensen@gmail.com>
2018-10-10 15:08:46 -0600

    incrased darkness of dialog background

--- src/components/CategoryGallery/CategoryBlock.js
+++ src/components/CategoryGallery/CategoryBlock.js
@@ -172,7 +172,7 @@ const DialogBackdrop = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
-  background: rgba(0, 0, 0, 0.2);
+  background: rgba(0, 0, 0, 0.4);
   z-index: ${props => props.theme.zIndexes.dialog};
   transition: 0.4s;
 `

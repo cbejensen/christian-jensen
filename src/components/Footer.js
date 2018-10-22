d873d6d0f7389012fdd75c2d96540f831bf3d0e7
Christian Jensen <cbejensen@gmail.com>
2018-10-22 15:20:11 -0600

    updated footer with icons and triangles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css, withTheme } from 'styled-components'
+import TriangleCurve from '../components/TriangleCurve'
+import { FileText, Mail, Smartphone } from 'react-feather'
 
 class Footer extends React.Component {
   render() {
     return (
       <Container>
-        <Link href="tel:+1-801-633-8055">801-633-8055</Link>
-        <Link href="mailto:cbejensen@gmail.com">cbejensen@gmail.com</Link>
+        <TriangleCurve
+          corner="bottom left"
+          height="200%"
+          width="40%"
+          triangleColor={this.props.theme.secondaryColor}
+          triangleSize="15-40"
+        />
+        <TriangleCurve
+          corner="bottom right"
+          height="200%"
+          width="40%"
+          triangleColor={this.props.theme.secondaryColor}
+          triangleSize="15-40"
+        />
+        <Row>
+          <Link href="tel:+1-801-633-8055">
+            <PhoneIcon />
+            801-633-8055
+          </Link>
+          <Link href="mailto:cbejensen@gmail.com">
+            <MailIcon />
+            cbejensen@gmail.com
+          </Link>
+        </Row>
+        <Link
+          href="https://res.cloudinary.com/cbejensen/image/upload/v1540242857/christian-jensen/resume-christian-jensen.pdf"
+          rel="external"
+          download
+        >
+          <FileTextIcon />
+          Resume
+        </Link>
       </Container>
     )
   }
@@ -14,19 +46,45 @@ class Footer extends React.Component {
 
 const Container = styled.footer`
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+  position: relative;
+  overflow: hidden;
   padding: 0.5em;
   background: ${props => props.theme.black};
 `
 
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`
+
+const Icon = css`
+  margin-right: 10px;
+  :hover {
+    color: inherit;
+  }
+`
+
+const PhoneIcon = styled(Smartphone)`
+  ${Icon};
+`
+const MailIcon = styled(Mail)`
+  ${Icon};
+`
+const FileTextIcon = styled(FileText)`
+  ${Icon};
+`
+
 const Link = styled.a`
-  display: block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 0.5em;
   min-width: 250px;
-  text-align: center;
   color: ${props => props.theme.white};
 `
 
-export default Footer
+export default withTheme(Footer)

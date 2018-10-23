f67e17de5464c1f52e388692d321d2610644d5fe
Christian Jensen <cbejensen@gmail.com>
2018-10-23 14:12:12 -0600

    removed unnecessary getData attr causing crash when trying to build

--- static.config.js
+++ static.config.js
@@ -9,8 +9,7 @@ export default {
   getRoutes: async () => [
     {
       path: '/',
-      component: 'src/pages/Home',
-      getData: async resolvedRoute => resolvedRoute
+      component: 'src/pages/Home'
     },
     {
       is404: true,
@@ -23,50 +22,39 @@ export default {
     meta.styleTags = sheet.getStyleElement()
     return html
   },
-  Document: class CustomHtml extends Component {
-    render() {
-      const { Html, Head, Body, children, renderMeta, siteData } = this.props
-      return (
-        <Html>
-          <Head>
-            <meta charSet="UTF-8" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/favicon-16x16.png"
-            />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link
-              rel="mask-icon"
-              href="/safari-pinned-tab.svg"
-              color={siteData.primaryColor}
-            />
-            <meta
-              name="msapplication-TileColor"
-              content={siteData.primaryColor}
-            />
-            <meta name="theme-color" content={siteData.primaryColor} />
-            {renderMeta.styleTags}
-          </Head>
-          <Body>{children}</Body>
-        </Html>
-      )
-    }
-  }
+  Document: ({ Html, Head, Body, children, renderMeta, siteData }) => (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color={siteData.primaryColor}
+        />
+        <meta name="msapplication-TileColor" content={siteData.primaryColor} />
+        <meta name="theme-color" content={siteData.primaryColor} />
+        {renderMeta.styleTags}
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  )
 }
